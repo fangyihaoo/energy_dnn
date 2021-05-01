6ca329053d8f6671228c29b188e074ea14cb91ee
HuZiqing <nicolas.huziqing@gmail.com>
2021-05-01 17:17:15 -0400

    modification

---
+++ models/basic_module.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import time
+
+
+class BasicModule(nn.Module):
+    """
+    Encapsulation for 'save' and 'load' method
+    """
+
+    def __init__(self):
+        super(BasicModule,self).__init__()
+        self.model_name=str(type(self)) # default name
+
+    def load(self, path):
+        """
+        load the model at specific path
+        """
+        self.load_state_dict(torch.load(path))
+
+    def save(self, name=None):
+        """
+        save the model with the default name
+        """
+        if name is None:
+            prefix = 'checkpoints/' + self.model_name + '_'
+            name = time.strftime(prefix + '%m%d_%H:%M:%S.pth')
+        t.save(self.state_dict(), name)
+        return name
+
+    def get_optimizer(self, lr, weight_decay):
+        return torch.optim.Adam(self.parameters(), lr=lr, weight_decay=weight_decay)
