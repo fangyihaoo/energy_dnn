e791fde8648b8bc0e4e0f952a0393d18a887f3d4
HuZiqing <nicolas.huziqing@gmail.com>
2021-05-17 17:08:16 -0400

    .

--- utils/lossfunc.py
+++ utils/lossfunc.py
@@ -30,17 +30,14 @@ def criterion(model, dat_i, dat_b):
         g = dat_i.clone()
         g.requires_grad = True
         output_g = model(g)
-        output_i = model(dat_i)
         output_b = model(dat_b)
-        ux = torch.autograd.grad(outputs = output_g, inputs = g, grad_outputs = torch.ones_like(output_g), retain_graph=True, create_graph=True)[0]
-        f = 2*torch.sin(dat_i[:,0])*torch.cos(dat_i[:,1])
-        
-        loss_i =  torch.mean(0.5 * torch.sum(torch.pow(ux, 2),dim=1)- 2*f*output_i)
-        loss_b = torch.mean(torch.pow(output_b,2))
-        
-        return loss_i + 500*loss_b        
+        du = torch.autograd.grad(outputs = output_g, inputs = g, grad_outputs = torch.ones_like(output_g), retain_graph=True, create_graph=True)[0]
+        Lu = torch.autograd.grad(outputs = du, inputs = g, grad_outputs = torch.ones_like(du), retain_graph=True, create_graph=True)[0]
+
+        loss_i = ((torch.sum(Lu, dim = 1) + 2*torch.sin(dat_i[:,0])*torch.cos(dat_i[:,1]))**2).mean()
+        loss_b = torch.pow(output_b, 2).mean()
+
+        return loss_i + 500*loss_b
 
     else:
         raise ValueError('No this model')
-
-
