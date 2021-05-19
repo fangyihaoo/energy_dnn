67e77a7ed8e7e43711522d29758d96b5ff64fe85
Ziqing Hu <zhu4@crcfe01.crc.nd.edu>
2021-05-19 16:39:25 -0400

    .

--- utils/lossfunc.py
+++ utils/lossfunc.py
@@ -20,7 +20,7 @@ def criterion(model, dat_i, dat_b, los = 'DRM'):
         ux = torch.autograd.grad(outputs = output_g, inputs = g, grad_outputs = torch.ones_like(output_g), retain_graph=True, create_graph=True)[0]
         f = 2*torch.sin(dat_i[:,0])*torch.cos(dat_i[:,1])
 
-        loss_i =  torch.mean(0.5 * torch.sum(torch.pow(ux, 2),dim=1)- 2*f*output_i)
+        loss_i =  torch.mean(0.5 * torch.sum(torch.pow(ux, 2),dim=1)- f*output_i)
         loss_b = torch.mean(torch.pow(output_b,2))
 
         return loss_i + 500*loss_b
